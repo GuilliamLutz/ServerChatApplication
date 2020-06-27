4cdec4fd702158440927302d83a13f5650cf9a00
Guilliam Lutz <guilliamlutz@outlook.com>
2020-06-27 20:29:01 +0200

    Automatic logging in implementation

--- Client/src/serverInterface.java
+++ Client/src/serverInterface.java
@@ -1,6 +1,4 @@
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
 import java.util.Scanner;
 
@@ -10,22 +8,42 @@ public class serverInterface {
     private Socket socket;
     private OutputStream serverOut;
     private InputStream serverIn;
+    private BufferedReader bufferedIn;
 
     public serverInterface(String serverName, int serverPort) {
         this.serverName = serverName;
         this.serverPort = serverPort;
     }
 
-    public static void main(String[] args) {
-        serverInterface serverInterface = new serverInterface("localhost", 8818);
-        if (!serverInterface.connect()){
+    public static void main(String[] args) throws IOException {
+        serverInterface client = new serverInterface("localhost", 8818);
+        if (!client.connect()){
             System.err.println("Connection failed");
         }else{
             System.out.println("Connection succesfull");
+            if (client.login("guest", "lul")){
+                System.out.println("Connected succesfully");
+            }else{
+                System.err.println("Connection failed");
+            }
         }
 
         Scanner s = new Scanner(System.in);
         s.next();
+    }
+
+    private boolean login(String name, String password) throws IOException {
+        String command = ("login " + name + " " + password + '\n');
+        this.serverOut.write(command.getBytes());
+        System.out.println("Login error for: " + name);
+        String serverResponse = this.bufferedIn.readLine();
+        System.out.println("Severresponse: " + serverResponse);
+
+        if (serverResponse.equals("Logged in!")){
+            return true;
+        }else {
+            return false;
+        }
     }
 
     private boolean connect() {
@@ -33,6 +51,7 @@ public class serverInterface {
             this.socket = new Socket(this.serverName, this.serverPort);
             this.serverOut = this.socket.getOutputStream();
             this.serverIn = this.socket.getInputStream();
+            this.bufferedIn = new BufferedReader(new InputStreamReader(this.serverIn));
             return true;
         } catch (IOException e) {
             e.printStackTrace();
